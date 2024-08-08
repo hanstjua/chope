d894a920257793d8eecc1d75cbe7ebda84b07354
Hanstjua <hanstjua@yahoo.co.id>
2024-08-08 21:36:11 +0800

    setting vars with kwargs

--- chope/css.py
+++ chope/css.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from itertools import chain
 from typing import Any, Dict, Iterable, List, Set, Union
 
 from chope.variable import Var
@@ -14,13 +15,15 @@ class Rule:
         self.__name = name
 
     def __eq__(self, __value: object) -> bool:
-        return self.__declarations == __value.__declarations \
+        return (
+            self.__declarations == __value.__declarations
             and self.__name == __value.__name
+        )
 
     def render(self, indent: int = 2) -> str:
-        nl = '\n'
+        nl = "\n"
         indented = indent > 0
-        declarations_str = ''
+        declarations_str = ""
 
         def get_value(var: Any) -> Any:
             if isinstance(var, Dict):
@@ -33,33 +36,40 @@ class Rule:
         declarations = get_value(self.__declarations)
 
         if not isinstance(declarations, dict):
-            raise RenderError(f"Invalid declaration {declarations} in rule '{self.__name}'. Declarations must be a dict object.")
-        
+            raise RenderError(
+                f"Invalid declaration {declarations} in rule '{self.__name}'. Declarations must be a dict object."
+            )
+
         for property, value in declarations.items():
             value = get_value(value)
             if isinstance(value, Iterable) and not isinstance(value, str):
-                value = ' '.join(value)
+                value = " ".join(value)
 
-            declarations_str += \
+            declarations_str += (
                 f'{" " * indent}{property.replace("_", "-")}: {value};{nl * indented}'
+            )
 
-        return f'{self.__name} {{{nl * indented}{declarations_str}}}'
-    
+        return f"{self.__name} {{{nl * indented}{declarations_str}}}"
+
     def get_vars(self) -> Set[str]:
         def get_var(var: Var) -> Set[str]:
             if isinstance(var.value, Var):
                 return {var.name}.union(get_var(var.value))
             else:
                 return {var.name}
-            
+
         if isinstance(self.__declarations, Var):
             return get_var(self.__declarations)
         elif isinstance(self.__declarations, dict):
-            return reduce(lambda out, s: out.union(s), (get_var(d) for d in self.__declarations.values()), set())
+            return reduce(
+                lambda out, s: out.union(s),
+                (get_var(d) for d in self.__declarations.values()),
+                set(),
+            )
         else:
             return set()
-    
-    def set_vars(self, values: Dict[str, Any]) -> 'Rule':
+
+    def set_vars(self, values: Dict[str, Any]) -> "Rule":
         def set_var(comp: Any, values: Dict[str, Any]) -> Any:
             if isinstance(comp, Var):
                 new_var = comp.set_value(values)
@@ -69,17 +79,26 @@ class Rule:
                     return new_var
             else:
                 return comp
-            
+
         if isinstance(self.__declarations, dict):
-            new_declarations = {attr : set_var(value, values) for attr, value in self.__declarations.items()}
+            new_declarations = {
+                attr: set_var(value, values)
+                for attr, value in self.__declarations.items()
+            }
         elif isinstance(self.__declarations, Var):
             old_var = self.__declarations
             new_var = set_var(old_var, values)
-            new_declarations = self if new_var == old_var else Var(old_var.name, new_var.value)
+            new_declarations = (
+                self if new_var == old_var else Var(old_var.name, new_var.value)
+            )
         else:
             new_declarations = self.__declarations
 
-        return self if new_declarations == self.__declarations else Rule(self.__name, new_declarations)
+        return (
+            self
+            if new_declarations == self.__declarations
+            else Rule(self.__name, new_declarations)
+        )
 
 
 class Css:
@@ -89,25 +108,30 @@ class Css:
     def __eq__(self, __value: object) -> bool:
         return self._rules == __value._rules
 
-    def __class_getitem__(cls, items: Union[slice, Iterable[slice]]) -> 'Css':
+    def __class_getitem__(cls, items: Union[slice, Iterable[slice]]) -> "Css":
         if isinstance(items, slice):
-            rules = [Rule(items.start.replace('_', '-'), items.stop)]
+            rules = [Rule(items.start.replace("_", "-"), items.stop)]
         else:
-            rules = [Rule(item.start.replace('_', '-'), item.stop)
-                     for item in items]
+            rules = [Rule(item.start.replace("_", "-"), item.stop) for item in items]
 
         return cls(rules)
 
     def render(self, indent: int = 2) -> str:
         indented = indent > 0
-        return ('\n\n' * indented).join((rule.render(indent) for rule in self._rules))
-    
+        return ("\n\n" * indented).join((rule.render(indent) for rule in self._rules))
+
     def get_vars(self) -> Set[str]:
-        return reduce(lambda out, s: out.union(s), (rule.get_vars() for rule in self._rules), set())
-    
-    def set_vars(self, values: Dict[str, Any]) -> 'Css':
+        return reduce(
+            lambda out, s: out.union(s),
+            (rule.get_vars() for rule in self._rules),
+            set(),
+        )
+
+    def set_vars(self, values_: Dict[str, Any], **kwargs) -> "Css":
+        combined_values = {k: v for k, v in chain(values_.items(), kwargs.items())}
+
         # shoutout to Dua Lipa
-        new_rules = [rule.set_vars(values) for rule in self._rules]
+        new_rules = [rule.set_vars(combined_values) for rule in self._rules]
         return self if new_rules == self._rules else Css(new_rules)
 
 
@@ -119,18 +143,18 @@ class Unit:
         return str(value) + self.__name
 
 
-cm = Unit('cm')
-ch = Unit('ch')
-em = Unit('em')
-ex = Unit('ex')
-in_ = Unit('in')
-mm = Unit('mm')
-pc = Unit('pc')
-percent = Unit('%')
-pt = Unit('pt')
-px = Unit('px')
-rem = Unit('rem')
-vh = Unit('vh')
-vmax = Unit('vmax')
-vmin = Unit('vmin')
-vw = Unit('vw')
+cm = Unit("cm")
+ch = Unit("ch")
+em = Unit("em")
+ex = Unit("ex")
+in_ = Unit("in")
+mm = Unit("mm")
+pc = Unit("pc")
+percent = Unit("%")
+pt = Unit("pt")
+px = Unit("px")
+rem = Unit("rem")
+vh = Unit("vh")
+vmax = Unit("vmax")
+vmin = Unit("vmin")
+vw = Unit("vw")
